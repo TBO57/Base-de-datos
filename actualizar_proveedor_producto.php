2daf57e2d3f56229884213747f4fe2d7c62ac398
TBO57 <emadridr05@gmai.com>
2022-02-21 07:25:03 -0500

    XD

--- actualizar_proveedor_producto.php
+++ actualizar_proveedor_producto.php
@@ -1,67 +1,69 @@
-<?php 
+<?php
 
 include("conexion.php");
 
-$con=conectar();
+$con = conectar();
 
-// $C_producto = $_GET['Cod_pr'];
 $Id = $_GET['Id_t'];
-// $C_producto = $_REQUEST['Cod_pr'];
-// $Id_prov = $_REQUEST['Id_pro'];
 
-$sql="SELECT * FROM proveedor_producto WHERE Id='$Id'";
 
-$query=mysqli_query($con,$sql);
+$sql = "SELECT * FROM proveedor_producto WHERE Id='$Id'";
 
-$row=mysqli_fetch_array($query);
+$query = mysqli_query($con, $sql);
+
+$row = mysqli_fetch_array($query);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <b><title>Actualizar proveedor - producto</title></b>
+    <b>
+        <title>Actualizar proveedor - producto</title>
+    </b>
     <link href="https://cdn.jsdelivr.net/npm/bootstrapp.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha284-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/actualizar.css">
+    <link rel="stylesheet" href="css/actualizar2.css">
 
 </head>
 
 
 <body>
 
-<div class="title text-center py-3">
-    <h1>Actualizando proveedor - producto...</h1> 
-</div>
+    <div class="title text-center py-3">
+        <h1>Actualizando proveedor - producto...</h1>
+    </div>
 
     <div class="tarjeta">
 
-     <div class="row justify-content-center">
-      <div style="display: flex; align-items:center;">
-        <form action="up_proveedor_producto.php?$row['Cod_pr']&$row['Id_pro']&$row['Costo']&$row['Fecha']&$row['Cantidad']" method="POST">
-        <p class="parr mt-2">Codigo</p> 
-         <input type="number" readonly class="form-control mb-2" name="Id" placeholder="ide" value="<?php echo $Id  ?>">
-         <p class="parr mt-2">Codigo producto:</p> 
-         <input type="text" readonly class="form-control mb-2" name="Cod_pr" placeholder="Codigo producto" value="<?php echo $row['Codigo_producto']  ?>">
-         <p class="parr mt-2">Id proveedor:</p> 
-         <input type="text" readonly class="form-control mb-2" name="Id_pro" placeholder="Id proveedor" value="<?php echo $row['Id_proveedor']   ?>">
-         <p class="parr mt-2">Costo total:</p> 
-         <input type="text" class="form-control mb-2" name="Costo" placeholder="Costo total" value="<?php  echo $row['Costo_total']  ?>">   
-         <p class="parr mt-2">Fecha compra:</p>           
-         <input type="text" class="form-control mb-2" name="Fecha" placeholder="Fecha" value="<?php  echo $row['Fecha_compra']  ?>"> 
-         <p class="parr mt-2">Cantidad producto:</p> 
-         <input type="text" class="form-control mb-2" name="Cantidad" placeholder="Cantidad" value="<?php  echo $row['Cantidad_producto']  ?>">     
-         <div class="parr mt-2">     
-         <input type="submit" class="btn btn-success mb-2 px-4" value="Actualizar">
-         </div>
-        </form>
-       
-      </div> 
-     </div>
+        <div class="row justify-content-center">
+            <div style="display: flex; align-items:center;">
+                <form action="up_proveedor_producto.php?$row['Cod_pr']&$row['Id_pro']&$row['Costo']&$row['Fecha']&$row['Cantidad']" method="POST">
+                    <p class="parr mt-2">Codigo</p>
+                    <input type="number" readonly class="form-control mb-2" name="Id" placeholder="ide" value="<?php echo $Id  ?>">
+                    <p class="parr mt-2">Codigo producto:</p>
+                    <input type="text" readonly class="form-control mb-2" name="Cod_pr" placeholder="Codigo producto" value="<?php echo $row['Codigo_producto']  ?>">
+                    <p class="parr mt-2">Id proveedor:</p>
+                    <input type="text" readonly class="form-control mb-2" name="Id_pro" placeholder="Id proveedor" value="<?php echo $row['Id_proveedor']   ?>">
+                    <p class="parr mt-2">Costo total:</p>
+                    <input type="text" class="form-control mb-2" name="Costo" placeholder="Costo total" value="<?php echo $row['Costo_total']  ?>">
+                    <p class="parr mt-2">Fecha compra:</p>
+                    <input type="text" class="form-control mb-2" name="Fecha" placeholder="Fecha" value="<?php echo $row['Fecha_compra']  ?>">
+                    <p class="parr mt-2">Cantidad producto:</p>
+                    <input type="text" class="form-control mb-2" name="Cantidad" placeholder="Cantidad" value="<?php echo $row['Cantidad_producto']  ?>">
+                    <div class="parr mt-2">
+                        <input type="submit" class="btn btn-success mb-2 px-4" value="Actualizar">
+                    </div>
+                </form>
+
+            </div>
+        </div>
     </div>
 
 
 </body>
+
 </html>
